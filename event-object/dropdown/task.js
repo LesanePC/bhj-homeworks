f240d7ddc3f4780dc2ef7c7446b344be2a578aa6
Eugene Fokin <evgen94@bk.ru>
2025-01-28 13:02:39 +0300

    First Task correct

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,12 +1,17 @@
 const dropdownElement = document.getElementsByClassName('dropdown__value')[0]
-dropdownElement.addEventListener('click', function (event) {
-    document.getElementsByClassName('dropdown__list')[0].classList.toggle('dropdown__list_active')
-    const items = document.getElementsByClassName('dropdown__item')
-    for (let i = 0; i < items.length; i++){
-        items[i].addEventListener('click', function (event){
-            event.target.closest('ul').previousElementSibling.innerHTML = event.target.firstElementChild.textContent
-            document.getElementsByClassName('dropdown__list')[0].classList.remove('dropdown__list_active')
-        })
-    }
-
+dropdownElement.addEventListener('click', function(event) {
+	event.preventDefault();
+	document.getElementsByClassName('dropdown__list')[0].classList.toggle('dropdown__list_active')
+	const items = document.getElementsByClassName('dropdown__item')
+	for (let i = 0; i < items.length; i++) {
+		items[i].addEventListener('click', function(event) {
+			event.preventDefault();
+			const selectedItem = event.target.closest('.dropdown__item');
+			if (selectedItem) {
+				const itemText = selectedItem.querySelector('a')?.textContent || selectedItem.textContent;
+				selectedItem.parentNode.previousElementSibling.innerHTML = itemText;
+				document.getElementsByClassName('dropdown__list')[0].classList.remove('dropdown__list_active');
+			}
+		})
+	}
 })
